e818c699903e42faefac0c77332ea66f2c537553
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-08-15 11:25:16 +0700

    line menu

--- js.js
+++ js.js
@@ -7,5 +7,31 @@ $( document ).ready(function() {
         $(".nav__overlay").toggle();
         $(".header__button--container").toggleClass("fixed__menu")
       });
-   
+
+      $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.gotop').fadeIn();
+        } else {
+            $('.gotop').fadeOut();
+        }
+    });
+
+  
+    const menus =  document.querySelectorAll(".header__menu--item")
+    const menuActive =document.getElementById("line-active")
+    const line =document.getElementById("menu-line")
+    line.style.left =menuActive.offsetLeft + "px";
+    line.style.width =  menuActive.offsetWidth + "px";
+    
+    menus.forEach((menu, index) => {
+
+        menu.onclick = function () {
+        line.style.left = this.offsetLeft + "px";
+        line.style.width = this.offsetWidth + "px";
+    
+
+      };
+    });
+
+
  });
